cb3f5924cc05de5c5e1501161a20d59d03943b3b
dmccarthy <dmccarthy@tssg.org>
2015-01-20 00:38:27 +0000

    removed auth bucket

--- core_bootstrap/security_framework_bootstrap.sh
+++ core_bootstrap/security_framework_bootstrap.sh
@@ -5,7 +5,6 @@ cd $SCRIPT_ROOT_DIR
 
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=users          --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=clients        --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
-sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=authorizations --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=dbkeys         --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 
 sudo mkdir /opt/n1ql
