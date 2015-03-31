1fdb58fdc9a339429774c07a1125caf9891393b1
dmccarthy <dmccarthy@tssg.org>
2015-03-31 10:36:53 +0100

    Added user-dash and auth dialog to provision and tmux scripts

--- core_provision/cloudlet_framework_provision.sh
+++ core_provision/cloudlet_framework_provision.sh
@@ -6,7 +6,9 @@ cd ~/repos
 git clone https://github.com/OPENi-ict/cloudlet-platform.git
 git clone https://github.com/OPENi-ict/mongrel2.git
 git clone https://github.com/OPENi-ict/admin-dashboard.git
+git clone https://github.com/OPENi-ict/user-dashboard.git
 
+cd ~/repos/user-dashboard; npm install --no-bin-links
 cd ~/repos/admin-dashboard; npm install --no-bin-links
 cd ~/repos/cloudlet-platform; npm install --no-bin-links
 
