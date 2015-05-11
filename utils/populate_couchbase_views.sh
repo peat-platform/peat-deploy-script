4a1ac219fb0f7457db0cf7ef8f416ba95b26eac3
dmccarthy <dmccarthy@tssg.org>
2015-05-11 22:25:22 +0100

    Attempt to solve utf-8 issue

--- utils/populate_couchbase_views.sh
+++ utils/populate_couchbase_views.sh
@@ -4,25 +4,7 @@
 curl  -X PUT \
   -H "Accept:application/json" \
   -H "Content-Type: application/json" \
-  -d '{
-      "views": { 
-         "object_by_cloudlet_id": {
-            "map": "function (doc, meta) {\n\n  if (undefined === doc[\"@type\"]){    return   }\n\n  var ts = new Date(doc[\"_date_modified\"]).getTime()\n\n  emit( [doc[\"@cloudlet\"], doc[\"@cloudlet\"], ts], doc[\"@id\"] );\n\n  for ( i in doc._permissions){\n\n    if ( doc._permissions[i][\"read\"] ){\n  	emit( [i, doc[\"@cloudlet\"], ts], doc[\"@id\"] );\n    }\n  }\n}",
-            "reduce":"_count"
-         },
-         "object_by_type" : {
-            "map" : "function (doc, meta) {\n if (undefined === doc[\"@type\"]){\n return \n }\n var ts = new Date(doc[\"_date_modified\"]).getTime() \n emit( [doc[\"@cloudlet\"], doc[\"@type\"], ts, doc[\"@cloudlet\"]], [doc[\"@cloudlet\"], doc[\"@id\"]] ); \n for ( i in doc._permissions){ \n if ( doc._permissions[i][\"read\"] ){ \n emit( [i, doc[\"@type\"], ts, doc[\"@cloudlet\"]], [doc[\"@cloudlet\"], doc[\"@id\"]] );\n }\n}\n}",
-            "reduce" : "_count"
-         },
-         "type_usage" : {
-            "map" : "function (doc, meta) {\n if (undefined === doc[\"@type\"]){\n    return \n  }\n emit(doc[\"@type\"], 1);\n}",
-            "reduce" : "_count"
-         },
-         "object_data" : {
-            "map" : "function (doc, meta) {\n if (undefined === doc[\"@type\"]){\n    return \n  }\n emit(doc[\"@id\"], doc[\"@type\"]);\n}"
-         }
-      }
-   }' \
+  -d '{"views": {"object_by_cloudlet_id": {"map": "function (doc, meta) {\n\n  if (undefined === doc[\"@type\"]){    return   }\n\n  var ts = new Date(doc[\"_date_modified\"]).getTime()\n\n  emit( [doc[\"@cloudlet\"], doc[\"@cloudlet\"], ts], doc[\"@id\"] );\n\n  for ( i in doc._permissions){\n\n    if ( doc._permissions[i][\"read\"] ){\n  	emit( [i, doc[\"@cloudlet\"], ts], doc[\"@id\"] );\n    }\n  }\n}","reduce":"_count"},"object_by_type" : {"map" : "function (doc, meta) {\n if (undefined === doc[\"@type\"]){\n return \n }\n var ts = new Date(doc[\"_date_modified\"]).getTime() \n emit( [doc[\"@cloudlet\"], doc[\"@type\"], ts, doc[\"@cloudlet\"]], [doc[\"@cloudlet\"], doc[\"@id\"]] ); \n for ( i in doc._permissions){ \n if ( doc._permissions[i][\"read\"] ){ \n emit( [i, doc[\"@type\"], ts, doc[\"@cloudlet\"]], [doc[\"@cloudlet\"], doc[\"@id\"]] );\n }\n}\n}","reduce" : "_count"},"type_usage" : {"map" : "function (doc, meta) {\n if (undefined === doc[\"@type\"]){\n    return \n  }\n emit(doc[\"@type\"], 1);\n}","reduce" : "_count"},"object_data" : {"map" : "function (doc, meta) {\n if (undefined === doc[\"@type\"]){\n    return \n  }\n emit(doc[\"@id\"], doc[\"@type\"]);\n}"}}}' \
   http://admin:password@localhost:8092/objects/_design/objects_views
 
 
