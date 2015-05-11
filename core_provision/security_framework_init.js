5d20c9223b27b042e8d09d7b48726e3d52fbff6b
dmccarthy <dmccarthy@tssg.org>
2015-05-11 23:32:18 +0100

    Added set db key

---
+++ core_provision/security_framework_init.js
@@ -0,0 +1,18 @@
+
+var couchbase  = require('couchbase');
+
+cluster = new couchbase.Cluster( 'couchbase://localhost' );
+
+db = cluster.openBucket('dbkeys');
+
+
+var create = function() {
+   db.insert('dbkeys_29f81fe0-3097-4e39-975f-50c4bf8698c7',
+      { "dbs": [ "users", "clients", "authorizations", "queries" ] },
+      {}, function (err, result) {
+         console.log('Success: ', 'Document Created.')
+         process.exit(code=0)
+      });
+}
+
+create();
--- core_provision/security_framework_provision.sh
+++ core_provision/security_framework_provision.sh
@@ -5,3 +5,7 @@ cd ~/repos
 git clone https://github.com/peat-platform/auth-dialogs.git
 
 cd ~/repos/auth-dialogs; npm install --no-bin-links
+
+npm install couchbase
+node /vagrant/core_provision/security_framework_init.js
+rm -r node_modules
