6d299ff20a095b1a859609bb424c2b88e9b50506
dmccarthy <dmccarthy@tssg.org>
2015-05-11 16:49:19 +0100

    tweaked installation

--- core_bootstrap/security_framework_bootstrap.sh
+++ core_bootstrap/security_framework_bootstrap.sh
@@ -8,12 +8,3 @@ sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=u
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=clients        --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=dbkeys         --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 
-sudo mkdir /opt/n1ql
-sudo chown -R $USER:$GROUP /opt/n1ql
-cd /opt/n1ql
-
-wget https://s3.amazonaws.com/query-dp3/couchbase-query_dev_preview3_x86_64_linux.tar.gz
-
-tar -xvf couchbase-query_dev_preview3_x86_64_linux.tar.gz
-
-sudo chown -R $USER:$GROUP /opt/n1ql
