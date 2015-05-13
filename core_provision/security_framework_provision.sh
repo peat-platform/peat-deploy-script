cd153c3b89da4afe195ee5d2375d5a830fd8d08f
dmccarthy <dmccarthy@tssg.org>
2015-05-13 16:04:21 +0100

    Fixed incorrect path

--- core_provision/security_framework_provision.sh
+++ core_provision/security_framework_provision.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+cd core_provision/
+npm install couchbase
+node core_provision/security_framework_init.js
+rm -r node_modules
+
 cd ~/repos
 
 git clone https://github.com/peat-platform/auth-dialogs.git
 
 cd ~/repos/auth-dialogs; npm install --no-bin-links
 
-cd core_provision/
-npm install couchbase
-node core_provision/security_framework_init.js
-rm -r node_modules
