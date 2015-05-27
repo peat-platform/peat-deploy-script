6a6ae4d94b2bb583d96cb48fa0f138a6fbd1e55e
Neil <nflynn@tssg.org>
2015-05-27 12:55:18 +0100

    Fixed typo.

--- core_provision/sdk_framework_provision.sh
+++ core_provision/sdk_framework_provision.sh
@@ -2,6 +2,6 @@
 
 cd ~/repos
 
-git clone https://github.com/peat-platform/peat-android-sdk
+git clone https://github.com/peat-platform/peat-android-sdk.git
 
 cd ~/repos/peat-android-sdk; bash setup.sh
